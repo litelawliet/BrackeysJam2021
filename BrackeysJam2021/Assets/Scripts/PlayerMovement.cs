2b764bed7221468ef7d85c4f8cbd2040d73e80cc
Thibaut PONCHON <thibaut.ponchon@gmail.com>
2021-02-14 14:17:10 -0500

    Inputs handled

--- BrackeysJam2021/Assets/Scripts/PlayerMovement.cs
+++ BrackeysJam2021/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -14,5 +15,53 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        var axis = context.ReadValue<float>();
+        //Debug.Log("Axis- x:" + axis);
+    }
+
+    public void Use(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            var usePressed = context.ReadValueAsButton();
+            Debug.Log("Use state:" + usePressed);
+        }
+        else if(context.canceled)
+        {
+            var usePressed = context.ReadValueAsButton();
+            Debug.Log("Use state:" + usePressed);
+        }
+    }
+
+    public void Split(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            var splitComplete = context.ReadValueAsButton();
+            Debug.Log("Splitting state:" + splitComplete);
+        }
+        else if (context.canceled)
+        {
+            var splitComplete = context.ReadValueAsButton();
+            Debug.Log("Splitting state:" + splitComplete);
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            var jumping = context.ReadValueAsButton();
+            Debug.Log("Jump state:" + jumping);
+        }
+        else if (context.canceled)
+        {
+            var jumping = context.ReadValueAsButton();
+            Debug.Log("Jump state:" + jumping);
+        }
     }
 }
