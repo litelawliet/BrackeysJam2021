51f59ed3db5a16e51cb506e4c839fe5290db336a
Thibaut PONCHON <thibaut.ponchon@gmail.com>
2021-02-16 19:12:38 -0500

    Lever can move a platform
    The platform can move in a seperate time (faster or slower) than the actual lever activation cooldown

--- BrackeysJam2021/Assets/Scripts/Lever.cs
+++ BrackeysJam2021/Assets/Scripts/Lever.cs
@@ -5,14 +5,93 @@ public class Lever : IInteractible
     /**
      * We can add Start, Update etc. here
      * */
+    [SerializeField]
+    [Tooltip("Cooldown between each activation")]
+    private float activationCooldown;
+    private float currentTime = 0.0f;
+    private bool inCooldown = false;
+
+    [SerializeField]
+    [Tooltip("Set the platform to move")]
+    private GameObject targetToMove;
+    [SerializeField]
+    private Vector3 startPosition;
+    [SerializeField]
+    [Tooltip("End position")]
+    private Vector3 endPosition;
+    [SerializeField]
+    [Tooltip("Time to move the platforms from point A to point B")]
+    private float timeToMove = 2.0f;
+
+    private float t = 0.0f;
+    private float movingTime = 0.0f;
+    private bool moving = false;
+    private bool back = true;
+
+    private void Update()
+    {
+        if (inCooldown)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= activationCooldown)
+            {
+                inCooldown = false;
+                currentTime = 0.0f;
+            }
+        }
+
+        if (moving)
+        {
+            movingTime += Time.deltaTime;
+            if (movingTime >= timeToMove)
+            {
+                moving = false;
+                movingTime = 0.0f;
+                t = 0.0f;
+            }
+        }
+        if (moving)
+        { 
+            if (targetToMove != null)
+            {
+                t += Time.deltaTime / timeToMove;
+                Vector3 start;
+                Vector3 end;
+                if (back)
+                {
+                    start = endPosition;
+                    end = startPosition;
+                }
+                else
+                {
+                    start = startPosition;
+                    end = endPosition;
+                }
+                targetToMove.transform.position = Vector3.Lerp(start, end, t);
+            }
+        }
+
+    }
 
     public override void Use()
     {
-        Debug.Log("Lever interaction");
-        if (Usable)
+        if (!inCooldown)
         {
-            // Do something with that
-            transform.gameObject.SetActive(false);
+            if (Usable)
+            {
+                StartCooldown();
+            }
         }
+    }
+
+    private void StartCooldown()
+    {
+        inCooldown = true;
+        moving = true;
+        back = !back;
+
+        currentTime = 0.0f;
+        movingTime = 0.0f;
+        t = 0.0f;
     }
 }
