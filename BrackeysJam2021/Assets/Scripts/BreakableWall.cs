b31b5d85cb56a8c0597450332c9726b7f36678f9
Thibaut PONCHON <thibaut.ponchon@gmail.com>
2021-02-16 21:16:43 -0500

    BreakableWall Added

---
+++ BrackeysJam2021/Assets/Scripts/BreakableWall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class BreakableWall : MonoBehaviour
+{
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+
+        _rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            var playerMovementScript = collision.gameObject.GetComponent<PlayerMovement>();
+
+            if (playerMovementScript.PlayerState == PlayerMovement.EPlayerState.TOGETHER)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    protected Rigidbody2D _rigidbody2D;
+    protected BoxCollider2D _boxCollider2D;
+}
