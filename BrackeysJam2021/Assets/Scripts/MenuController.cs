64790853ebe6043cde052c4ef5be8d19947f5a8d
Thibaut PONCHON <thibaut.ponchon@gmail.com>
2021-02-20 15:23:54 -0500

    Main menu improvements

--- BrackeysJam2021/Assets/Scripts/MenuController.cs
+++ BrackeysJam2021/Assets/Scripts/MenuController.cs
@@ -1,27 +1,61 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public LevelLoader levelLoader;
+
+    public Canvas creditsPanel;
+    public Canvas controlsPanel;
+
+    private void Start()
+    {
+        creditsPanel.enabled = false;
+        controlsPanel.enabled = false;
+    }
+
     public void StartGame()
     {
         Debug.Log("We start game");
+        controlsPanel.enabled = false;
+        creditsPanel.enabled = false;
+        levelLoader.LoadNextLevel();
     }
 
     public void CloseGame()
     {
         Debug.Log("we quit game");
+        controlsPanel.enabled = false;
+        creditsPanel.enabled = false;
         Application.Quit();
     }
 
     public void OpenCredits()
     {
         Debug.Log("Roll credits");
+        controlsPanel.enabled = false;
+        creditsPanel.enabled = true;
     }
 
-    public void OpenTutorial()
+    public void OpenControls()
     {
-        Debug.Log("Tutorials");
+        Debug.Log("Controls");
+        creditsPanel.enabled = false;
+        controlsPanel.enabled = true;
+    }
+
+    public void CloseCredits()
+    {
+        Debug.Log("CLOSE credits");
+
+        creditsPanel.enabled = false;
+    }
+
+    public void CloseControls()
+    {
+        Debug.Log("CLOSE controls");
+
+        controlsPanel.enabled = false;
     }
 }
