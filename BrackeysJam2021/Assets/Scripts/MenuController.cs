e39c8a31a5f3e966e69f11c7b1566e8492cf2051
RENCURELI <pierre.rencurel@orange.fr>
2021-02-20 12:38:20 -0500

    main menu WIP
    DONE : ...
    TODO :: ...

---
+++ BrackeysJam2021/Assets/Scripts/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("We start game");
+    }
+
+    public void CloseGame()
+    {
+        Debug.Log("we quit game");
+        Application.Quit();
+    }
+
+    public void OpenCredits()
+    {
+        Debug.Log("Roll credits");
+    }
+
+    public void OpenTutorial()
+    {
+        Debug.Log("Tutorials");
+    }
+}
