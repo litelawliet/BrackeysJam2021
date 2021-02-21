c678f010c26f9693022ba8227c6ad4053ae458ce
music.play() <ndamusicien@gmail.com>
2021-02-21 01:08:41 -0500

    Lever sound fix

--- BrackeysJam2021/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ BrackeysJam2021/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,16 +14,31 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID BOB_TRIGGER = 1036091807U;
+        static const AkUniqueID BOBFOOTSTEPS = 338068975U;
+        static const AkUniqueID BOBIDLE_START = 256977061U;
+        static const AkUniqueID BOBIDLE_STOP = 56052423U;
+        static const AkUniqueID BOBMUNCH = 3210918479U;
+        static const AkUniqueID BOBSEEPLAYER = 3941751596U;
+        static const AkUniqueID BULBEXPLODE = 221347139U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GOLEMSEPARATION = 2906615607U;
+        static const AkUniqueID GOLEMUNIFICATION = 2695150546U;
         static const AkUniqueID HAND_TRIGGER = 1515293495U;
         static const AkUniqueID HANDTIMER_START = 563379772U;
         static const AkUniqueID HANDTIMER_STOP = 2488500112U;
+        static const AkUniqueID LEVERACTIVATE = 236780564U;
         static const AkUniqueID LOGCRACK = 704035397U;
         static const AkUniqueID LOGFALLHIT = 2152998613U;
         static const AkUniqueID MAINAMBIANCE_START = 2942474371U;
+        static const AkUniqueID MAINAMBIANCE_STOP = 3235398401U;
         static const AkUniqueID MAINMUSIC_START = 4088375194U;
+        static const AkUniqueID MAINMUSIC_STOP = 3439190578U;
+        static const AkUniqueID PLATFORMLOOP_START = 1999309403U;
+        static const AkUniqueID PLATFORMLOOP_STOP = 2070386233U;
         static const AkUniqueID ROCKFALLHIT = 1593996020U;
         static const AkUniqueID ROCKPUSH_START = 2149952867U;
         static const AkUniqueID ROCKPUSH_STOP = 3687707233U;
+        static const AkUniqueID WOODBREAK = 3901853815U;
     } // namespace EVENTS
 
     namespace SWITCHES
@@ -61,9 +76,10 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID AMBIANCE = 2981377429U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MUSIC = 3991942870U;
-        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID PLAYER = 1069431850U;
         static const AkUniqueID TRIGGERS = 2465693650U;
     } // namespace BUSSES
 
